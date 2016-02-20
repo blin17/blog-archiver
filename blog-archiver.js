dd25693eb008444542f64bfcc4006f0b4ff5008b
Brian Lin <blin17@gmail.com>
2016-02-19 19:10:27 -0500

    fixed DS_STORE bug

--- blog-archiver.js
+++ blog-archiver.js
@@ -29,6 +29,7 @@ BlogArchiver.generateHTML = function(title, directory, options){
 	}
 	options = options || {};
 	var routing = options.routing || "/blog";
+	var extension = options.extension || "md";
 	var capitalize = options.capitalize || true;
 	var files = fs.readdirSync(directory);
 	files.sort(function(a, b) {
@@ -43,31 +44,33 @@ BlogArchiver.generateHTML = function(title, directory, options){
 	html_string += ("<ul class = \"posts\">\n");
 	for (var i = 0; i < files.length; i ++){
 		fn_split = files[i].split("-");
-		if (fn_split.length >= 4){
-			var date = month_conversionENUM[parseInt(fn_split[1])] + " " + fn_split[2] + " " + fn_split[0];
-			var filename = fn_split[0] + "-" + fn_split[1] + "-" + fn_split[2];
-			var title = "";
-			var extension = "";
-			for (var t = 3; t <fn_split.length; t++){
-				var title_word = fn_split[t];
-				if (t == fn_split.length-1){
-					var separate_extension = fn_split[t].split(".");
-					title_word = separate_extension[0];
-					extension = separate_extension[1];
+		var ext = fn_split[fn_split.length-1].split(".")[1];
+		if (ext == extension){
+			if (fn_split.length >= 4){
+				var date = month_conversionENUM[parseInt(fn_split[1])] + " " + fn_split[2] + " " + fn_split[0];
+				var filename = fn_split[0] + "-" + fn_split[1] + "-" + fn_split[2];
+				var title = "";
+				for (var t = 3; t <fn_split.length; t++){
+					var title_word = fn_split[t];
+					if (t == fn_split.length-1){
+						var separate_extension = fn_split[t].split(".");
+						title_word = separate_extension[0];
+					}
+					filename += "-" + title_word;
+					title += (capitalize? capitalizeFirstLetter(title_word): title_word) + ((t == fn_split.length-1)? "":" ");
 				}
-				filename += "-" + title_word;
-				title += (capitalize? capitalizeFirstLetter(title_word): title_word) + ((t == fn_split.length-1)? "":" ");
+				html_string += "<li><span id = \"date\">";
+				html_string += date;
+				html_string += "</span> — <a href =\"";
+				html_string += path.join(routing,filename);
+				html_string += "\">";
+				html_string += title;
+				html_string += "</a></li>\n";
 			}
-			html_string += "<li><span id = \"date\">";
-			html_string += date;
-			html_string += "</span> — <a href =\"";
-			html_string += path.join(routing,filename);
-			html_string += "\">";
-			html_string += title;
-			html_string += "</a></li>\n";
-		}
-		else{
-			throw new Error("Invalid File Format for Blog");
+			else{
+				throw new Error("Trying to generate an archive page for the directory:" 
+						+ directory + "\nInvalid File Format for the file:"+ files[i]);
+			}
 		}
 	}
 	html_string += "</ul>";
@@ -76,20 +79,20 @@ BlogArchiver.generateHTML = function(title, directory, options){
 };
 
 month_conversionENUM = {
- 1: "Jan",
- 2: "Feb",
- 3: "Mar",
- 4: "Apr",
- 5: "May",
- 6: "Jun",
- 7: "Jul",
- 8: "Aug",
- 9: "Sep",
- 10: "Oct",
- 11: "Nov",
- 12: "Dec"
+	1: "Jan",
+	2: "Feb",
+	3: "Mar",
+	4: "Apr",
+	5: "May",
+	6: "Jun",
+	7: "Jul",
+	8: "Aug",
+	9: "Sep",
+	10: "Oct",
+	11: "Nov",
+	12: "Dec"
 };
 
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+	return string.charAt(0).toUpperCase() + string.slice(1);
 }
